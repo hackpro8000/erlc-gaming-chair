bce0f98e6b10bd619957ef1774429412f65c17cb
hackpro8000 <143388240+hackpro8000@users.noreply.github.com>
2023-08-28 17:54:01 +0800

    a

--- core.lua
+++ core.lua
@@ -1,6 +1,8 @@
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
 
+local LocalPlr = Players.LocalPlayer
+
 local Signal = loadstring(game:HttpGet('https://raw.githubusercontent.com/Sleitnick/RbxUtil/main/modules/signal/init.lua'))()
 local Trove = loadstring(game:HttpGet('https://raw.githubusercontent.com/Sleitnick/RbxUtil/main/modules/trove/init.lua'))()
 
@@ -15,7 +17,66 @@ core.properties = {
         carBooster = false,
     }
 }
+core.updateTasks = {}
+core.onPropertyChanged = Signal.new()
 
+
+
+function childAdded(inst, fn)
+    for _, v in pairs(inst:GetChildren()) do
+        fn(v)
+    end
+    return inst.ChildAdded:Connect(fn)
+end
+
+function getOwnVehicle()
+    for _, vehicle in pairs(workspace.Vehicles:GetChildren()) do
+        
+        local controlValues = vehicle:FindFirstChild("Control_Values")
+        local ownerId = controlValues and controlValues:FindFirstChild("OwnerId")
+
+        if ownerId then
+            if ownerId.Value == LocalPlr.UserId then
+                return vehicle
+            end
+        end
+
+    end
+end
+
+function handleVehicleCollisions(vehicle)
+    local ownVehicle = getOwnVehicle()
+    local isOwnVehicle = vehicle == ownVehicle
+
+    for _, basepart in pairs(vehicle:GetDescendants()) do
+        if basepart:IsA "BasePart" then
+            basepart.CanCollide = not isOwnVehicle and core.properties.carCollisionsEnabled
+        end
+    end
+end
+
+function onVehicleAdded(vehicle)
+    local ownVehicle = getOwnVehicle()
+
+    handleVehicleCollisions(vehicle)
+end
+
+function onVehicleCollisionChange()
+    handleVehicleCollisions()
+end
+
+function whenPropertyChanged(name, value, oldValue)
+    if name == "carCollisionsEnabled" then
+        onVehicleCollisionChange()
+    end
+end
+
+core._trove:Add(childAdded(workspace.Vehicles, onVehicleAdded))
+core._trove:Add(core.onPropertyChanged:Connect(whenPropertyChanged))
+
+
+
+-- core stuff below
 function getOldCore()
     local old_core = getgenv().gamingchairCore
 
@@ -40,9 +101,16 @@ function core.safeFunction(fn, shouldWait)
     end
 end
 
+core.changeProperty = core.safeFunction(function(property, value)
+    local oldValue = core.properties[property]
+    core.properties[property] = value
+    core.onPropertyChanged:Fire(property, value, oldValue)
+end)
+
 core.start = core.safeFunction(function()
     getgenv().gamingchairCore = core
 
+    core._trove:Add(core.onPropertyChanged)
     core._trove:Add(function()
         getgenv().gamingchairCore = core
     end)
@@ -53,7 +121,9 @@ core.start = core.safeFunction(function()
 end, true)
 
 core.update = core.safeFunction(function()
-    
+    for taskName, taskFn in pairs(core.updateTasks) do
+        taskFn()
+    end
 end)
 
 function core.clean()
--- interface.lua
+++ interface.lua
@@ -3,7 +3,7 @@ local UILib = loadstring(game:HttpGet('https://raw.githubusercontent.com/Loco-CT
 local core = getgenv().gamingchairCore
 local interface = {}
 
-function interface.start()
+function initWindow()
     interface.Window = UILib:Create {
         Name = "ERLC Gaming Chair",
         Footer = "What the fuck is a footer?!",
@@ -11,6 +11,35 @@ function interface.start()
     }
 
     core._trove:Add(interface.Window)
+end
+
+function addWindowContent()
+    do -- ito ay para sa trolling tab
+        local trollingTab = interface.Window:Tab {
+            Name = "Trolling",
+            Color = Color3.new(0.85, 0.5, 0),
+        }
+        interface.Window.trollingTab = trollingTab
+
+        local vehicleSection = trollingTab:Section {
+            Name = "Vehicles"
+        }
+        trollingTab.vehicleSection = vehicleSection
+        
+        vehicleCollisionsToggle = vehicleSection:Toggle {
+            Name = "Vehicle Collisions",
+            Default = true,
+            callback = function(on)
+                core.properties.carCollisionsEnabled = on
+            end
+        }
+    end
+    
+end
+
+function interface.start()
+    initWindow()
+    addWindowContent()
 end
 
 return interface
